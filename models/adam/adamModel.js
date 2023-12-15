f1b08895796b5efa3608be70390b76658de68505
MagicDev0428 <fullstackdever425@gmail.com>
2023-12-15 16:18:36 +0500

    removed validation from adam create

--- models/adam/adamModel.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-const { adamSchema } = require("../../schema/adam/adamSchema");
-
-module.exports.adamModel = mongoose.model("adam", adamSchema);
--- models/adamModel.js
+++ models/adamModel.js
@@ -1,33 +1,39 @@
 // Adam Model
 // This model is the structure containing all information about money to and from Adam
 //
-var mongoose   = require('mongoose');
-var Schema           = mongoose.Schema;
-var adamSchema = new Schema({
-    _id:                Number,     // Transaction Date and Time:
-    amount:             Number,     // Amount of THB transfered
-    transferFrom:       String,     // Transfer money FROM (Adam, Torben, Bee, Other) 
-    transferTo:         String,     // Transfer money TO (Adam, Torben, Bee, Other)
-    investorName:       String,     // Name from Investors Table 
-    investmentNo:       Number,     // Investment Number from Investment Table
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const adamSchema = new Schema(
+  {
+    _id: Number, // Transaction Date and Time:
+    amount: Number, // Amount of THB transfered
+    transferFrom: String, // Transfer money FROM (Adam, Torben, Bee, Other)
+    transferTo: String, // Transfer money TO (Adam, Torben, Bee, Other)
+    investorName: String, // Name from Investors Table
+    investmentNo: Number, // Investment Number from Investment Table
 
-    transactionFrom:    String,     // Transfer transaction FROM bank / account 
-    transactionTo:      String,     // Transfer transaction TO bank / account 
-    transactionNo:      String,     // Transaction number from bank 
+    transactionFrom: String, // Transfer transaction FROM bank / account
+    transactionTo: String, // Transfer transaction TO bank / account
+    transactionNo: String, // Transaction number from bank
 
-    description:        String,     // Description of what happened 
-    attachments:        Array,      // Array of Images and files ("filename","filename") containing transfer reciepts
+    description: String, // Description of what happened
+    attachments: Array, // Array of Images and files ("filename","filename") containing transfer reciepts
 
-    createdDate: {                  // When was this investor created
-        type: Date, 
-        default: Date.now
-    },                            
-    createdBy:          String,     // Who created the investor
-    modifiedDate: {                 // When was this investor modified
-        type: Date, 
-        default: Date.now
-    },     
-    modifiedBy:         String      // Who modified the investor
-    
-}, { versionKey: false });          // Don't want to insert _v in document
-module.exports = mongoose.model('adamModel', adamSchema,'adam');
+    createdDate: {
+      // When was this investor created
+      type: Date,
+      default: Date.now,
+    },
+    createdBy: String, // Who created the investor
+    modifiedDate: {
+      // When was this investor modified
+      type: Date,
+      default: Date.now,
+    },
+    modifiedBy: String, // Who modified the investor
+  },
+  { versionKey: false }
+); // Don't want to insert _v in document
+
+module.exports.adamModel = mongoose.model("adamModel", adamSchema, "adams");
+module.exports.adamSchema = adamSchema;
