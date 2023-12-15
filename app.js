7ce9dc21afdc2af901da0c803a87d3a8dc3ec00a
MagicDev0428 <fullstackdever425@gmail.com>
2023-12-15 19:27:47 +0700

    adding new db string

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ if (process.env.SERVER_NAME === "LIVE") {
   console.log("##        " + tmpDate + "        ##");
   console.log("##                                    ##");
   console.log("########################################");
-  global.db = "mongodb://localhost:27017/InvestorSystem";
+  global.db ="mongodb+srv://torben:0yemRa61w7FbnqDm@cluster0.zfsf0vx.mongodb.net/";
+  //global.db = "mongodb+srv://insram:insram@cluster0.u1nadii.mongodb.net/InvestorSystem"
+  //global.db = "mongodb://localhost:27017/InvestorSystem";
   global.state = "TEST";
   global.server = "http://localhost:3007";
   global.isLOCAL = true;
