ac3d916ddeab6f322f14859557292fc397e20bfb
MagicDev0428 <fullstackdever425@gmail.com>
2023-12-24 09:58:40 +0500

    update investor info

--- controllers/investor/investorsListInfo.js
+++ controllers/investor/investorsListInfo.js
@@ -6,24 +6,33 @@ import { Models } from "../../models";
 // creating investor table model
 let investorTable = Models.Investor;
 
-const buttonColor = (emailDate,deposit,profitMonth)=>{
-
-    const currentDate = new Date('2023-12-31T19:00:00.000Z');
+const buttonColor = (emailDate,deposit,profitMonth,profitMonthPaid,totalInvestment)=>{
+console.log(emailDate,deposit,profitMonth,profitMonthPaid,totalInvestment);
+    const currentDate = new Date();
     const options = { year: 'numeric', month: 'short' };
     const formattedDate = currentDate.toLocaleDateString('en-US', options);
     const profitMonthDate = profitMonth.toLocaleDateString('en-US', options);
-    if(formattedDate === profitMonthDate && deposit>0 && emailDate){
-      return "GREEN"
+
+    if(formattedDate === profitMonthDate && profitMonthPaid){   // checking the date is equal to this month/year and profitMonthPaid is true
+      
+      // the deposit is >= to totalInvestment and emailDate exist then color will be Green
+      if( deposit >= totalInvestment && emailDate){
+        return "GREEN"
+      }
+      // the deposit is >= to totalInvestment  and email date does not exist then color will be Yellow
+      else if( deposit >= totalInvestment  && !emailDate){
+      return "YELLOW"
     }
-    else if(formattedDate === profitMonthDate && deposit > 0 && !emailDate){
-    return "YELLOW"
-  }
-    else if(formattedDate === profitMonthDate && !deposit>0 && !emailDate){
-    return "RED"
-  }
-    else {
-      return "none"
+
     }
+    // if deposit is not >= totalInvestment and profitMonthPaid is false  then color will be RED
+    else if( !deposit >= totalInvestment  && !profitMonthPaid){
+      console.log("------- i am else ");
+      return "RED"
+    }
+   
+      return "RED"
+    
 }
 
 // Common function for the aggregation pipeline
@@ -31,8 +40,95 @@ const aggregateInvestorData = async (pipeline) => {
   return await Models.Investor.aggregate(pipeline);
 };
 
+
+// Inserting button color
+
+const insertingButtonColor = (data)=>{
+        data.forEach((investorInfo) => {
+        if(!investorInfo.investor.getInvestmentType){
+          investorInfo.investor.buttonColor = 'PURPLE'
+        }
+        else{
+              investorInfo.investor.accountBalances.balanceList.forEach(async(balance) => {
+              investorInfo.investor.buttonColor =  buttonColor(balance.emailDate,balance.deposit,balance.profitMonth,balance.profitMonthPaid,investorInfo.investor.totalInvestment)
+           });
+        }
+
+});
+return data;
+}
+
 // Common stages for the aggregation pipeline
 const commonStages = [
+
+  {
+    $lookup: {
+      from: "myInvestments",
+      let: { investor: "$_id" },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ["$investorName", "$$investor"] },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            totalInvested: { $sum: { $ifNull: ["$amountInvested", 0] }},
+            totalProfitMonthly: { $sum: { $ifNull: ["$profitMonthly", 0] } },
+           totalProfitEnd: { $sum: { $ifNull: ["$profitEnd", 0] } },
+            myInvestmentList: {
+                $push: "$$ROOT"
+            },
+          },
+        },
+      ],
+      as: "accountInvestments",
+    },
+  },
+  {
+    $unwind: {
+      path: "$accountInvestments",
+      preserveNullAndEmptyArrays: true,
+    },
+  },
+  {
+     $addFields: {
+      totalInvestment:"$accountInvestments.totalInvested",
+      totalMonthlyProfit:"$accountInvestments.totalProfitMonthly",
+      totalProfitEnd: "$accountInvestments.totalProfitEnd",
+      totalProfit: {
+        $add: ["$accountInvestments.totalInvested", "$accountInvestments.totalProfitMonthly", "$accountInvestments.totalProfitEnd"],
+      },
+      getInvestmentType:{
+          $cond: {
+              if: {
+                $eq: [
+                  {
+                    $size: {
+                      $filter: {
+                                     input: {
+                $ifNull: ['$accountInvestments.myInvestmentList', []],
+              },
+                        as: 'myInvestment',
+                        cond: {
+                          $or: [
+                            { $eq: ['$$myInvestment.investType', 'Mixed'] },
+                            { $eq: ['$$myInvestment.investType', 'Monthly Profit'] },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                  1,
+                ],
+              },
+              then: true,
+              else: false,
+        },
+      }
+     }
+  },
   {
     $lookup: {
       from: "balance",
@@ -69,37 +165,6 @@ const commonStages = [
     },
   },
   {
-    $lookup: {
-      from: "myInvestments",
-      let: { investor: "$_id" },
-      pipeline: [
-        {
-          $match: {
-            $expr: { $eq: ["$investorName", "$$investor"] },
-          },
-        },
-        {
-          $group: {
-            _id: null,
-            totalInvested: { $sum: "$amountInvested" },
-            totalProfitMonthly: { $sum: "$profitMonthly" },
-            myInvestmentList: {
-                $push: "$$ROOT"
-            },
-          },
-        },
-      ],
-      as: "accountInvestments",
-    },
-  },
-  {
-    $unwind: {
-      path: "$accountInvestments",
-      preserveNullAndEmptyArrays: true,
-    },
-  },
-
-  {
     $project: {
       investor: "$$ROOT",
     },
@@ -107,7 +172,6 @@ const commonStages = [
 ];
 
 
-// Use the commonStages in your aggregation query
 
 
 
@@ -144,12 +208,9 @@ exports.investorInfo = async (id) => {
     // checking if investor data exist then resolve the promise otherwise reject it
     if (investorTable) {
 
-     
-//       investorTable.forEach((investor) => {
-//         investor.investor.accountBalances.balanceList.forEach(async(balance) => {
-//            balance.buttonColor =  buttonColor(balance.emailDate,balance.deposit,balance.profitMonth)
-//      });
-// });
+      // calling a inserting button color which will insert color and return it
+      // investorTable = insertingButtonColor(investorTable);
+
       return resolve({ err: false, investors: investorTable });
     
     }
@@ -175,7 +236,13 @@ exports.investorList = async () => {
     investorTable = await aggregateInvestorData(pipeline); // calling aggregation function for investor list
 
     // checking if investor data exist then resolve the promise otherwise reject it
-    if (investorTable) {return resolve({ err: false, investors: investorTable });}
+    if (investorTable) {
+
+      // calling a inserting button color which will insert color and return it
+      // investorTable = insertingButtonColor(investorTable);
+   
+      return resolve({ err: false, investors: investorTable });
+    }
     return reject({ err: true, message: "Unable to get investor list!" });
     } catch (error) {
        return reject({err:true,message:error.message})
