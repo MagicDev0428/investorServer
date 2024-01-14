fd187a5654616facf2336438425ffe07bad3e224
MagicDev0428 <fullstackdever425@gmail.com>
2024-01-14 19:40:58 +0500

    get all list of invetors which have status ACTIVE

--- controllers/investor/investorList.js
+++ controllers/investor/investorList.js
@@ -346,7 +346,7 @@ const investmentAggregate = async () => {
                 from: "investor",
                 pipeline: [{
                         $match: {
-                            status: "INVESTOR"
+                            status: "ACTIVE"
                         }
                     },
                     {
