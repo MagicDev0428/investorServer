8085e71f7a200cdcb3d88fe8b5dbadd7a86e1066
MagicDev0428 <fullstackdever425@gmail.com>
2024-01-23 03:11:17 +0500

    added total amount invested in investment list

--- controllers/investment/investmentList.js
+++ controllers/investment/investmentList.js
@@ -10,9 +10,118 @@ import { Lib } from "../../utils";
 let investmentTable = Models.investmentModel;
 
 
+// const totalAmountInvestedAggregation =()=>
+// {
+//  return [
+//     {
+//         $lookup: {
+//             from: "myInvestments",
+//             let: {
+//                 investment: "$_id"
+//             },
+//             pipeline: [{
+//                     $match: {
+//                         $expr: {
+//                             $eq: ["$investmentNo", "$$investment"]
+//                         },
+//                     },
+//                 },
+//                 {
+//                     $group: {
+//                         _id: null,
+//                         totalAmountInvested:{
+//                             $sum: {
+//                                     $ifNull: ["$amountInvested", 0]
+//                                 }
+//                       },
+                        
+//                     },
+//                 },
+//             ],
+//             as: "myInvestments",
+//         },
+//     },
+//     {
+//         $unwind: {
+//             path: "$myInvestments",
+//             preserveNullAndEmptyArrays: true,
+//         },
+//     },
+//      {
+//             $addFields: {
+//                 totalAmountInvested: {
+//                     $ifNull: ["$myInvestments.totalAmountInvested", 0]
+//                 },
+//             },
+//         },
+//     {
+//         $project: {
+//             myInvestments: 0,
+//             investment: "$$ROOT",
+//         },
+//     },
+// ];
+// }
+
+
+
+
 //
 // List all from investment collection
 //
+const totalAmountInvestedAggregation = () => {
+    return [
+        {
+            $lookup: {
+                from: "myInvestments",
+                let: {
+                    investment: "$_id"
+                },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ["$investmentNo", "$$investment"]
+                            },
+                        },
+                    },
+                    {
+                        $group: {
+                            _id: null,
+                            totalAmountInvested: {
+                                $sum: {
+                                    $ifNull: ["$amountInvested", 0]
+                                }
+                            },
+                        },
+                    },
+                ],
+                as: "myInvestments",
+            },
+        },
+        {
+            $unwind: {
+                path: "$myInvestments",
+                preserveNullAndEmptyArrays: true,
+            },
+        },
+        {
+            $addFields: {
+                totalAmountInvested: {
+                    $ifNull: ["$myInvestments.totalAmountInvested", 0]
+                },
+                myInvestments:0
+            },
+        },
+        {
+            $project: {
+                investment: "$$ROOT", // Include the entire root document
+            },
+        },
+    ];
+};
+
+
 export const investmentList = () => {
     global.show("###### List of Investments ######");
 
@@ -21,17 +130,18 @@ export const investmentList = () => {
 
              
             investmentTable = null;
-            investmentTable = await Models.investmentModel.find().sort({
-                _id: 1
-            }).lean();
+            investmentTable = await Models.investmentModel.aggregate([...totalAmountInvestedAggregation()])
+            // investmentTable = await Models.investmentModel.find().sort({
+            //     _id: 1
+            // }).lean();
 
 
             if (investmentTable ) {               
                 // adding new property investment ends 
                 investmentTable = investmentTable.map(investment_ => ({
                     
-                    ...investment_,
-                    investmentEnds: Lib.calculateDays(investment_.startDate, investment_.endDate)
+                    ...investment_.investment,
+                    investmentEnds: Lib.calculateDays(investment_.investment.startDate, investment_.investment.endDate)
                 }));
                 return resolve({
                     err: false,
